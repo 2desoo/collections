0ca23cdcccf20224798755250bac5222d934acdb
123 <marcus60.2001@gmail.com>
2024-02-05 15:37:15 +0300

    Initial commit

---
+++ src/test/java/collection/list/homework/lists/ApplicationTests.java
@@ -0,0 +1,13 @@
+package collection.list.homework.lists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
